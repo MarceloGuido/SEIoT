1cf2088a12e3615f6497b7fa900d1a66ce0f5406
Marcelo Guido <prof.marceloguido@gmail.com>
2026-06-18 17:03:24 -0300

    Update webServer.cpp

--- webServer.cpp
+++ webServer.cpp
@@ -14,3 +14,55 @@ void iniciarServidor()
 
     Serial.println("Servidor iniciado");
 }
+
+void processarServidor()
+{
+    WiFiClient client = server.available();
+
+    if(!client)
+        return;
+
+    while(client.connected())
+    {
+        if(client.available())
+        {
+            char c = client.read();
+
+            header += c;
+
+            if(c == '\n')
+            {
+                if(header.indexOf("GET /5/on") >= 0)
+                    ligarD1();
+
+                else if(header.indexOf("GET /5/off") >= 0)
+                    desligarD1();
+
+                else if(header.indexOf("GET /4/on") >= 0)
+                    ligarD2();
+
+                else if(header.indexOf("GET /4/off") >= 0)
+                    desligarD2();
+
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-type:text/html");
+                client.println();
+
+                client.println(
+                    gerarPagina(
+                        estadoD1(),
+                        estadoD2()
+                    )
+                );
+
+                client.println();
+
+                break;
+            }
+        }
+    }
+
+    header = "";
+
+    client.stop();
+}
