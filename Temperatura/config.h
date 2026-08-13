429f2e610d4d0e816d560207c8f20098e529baee
Marcelo Guido <prof.marceloguido@gmail.com>
2026-08-13 19:24:05 -0300

    Update config.h
    Correção de dados

--- Temperatura/config.h
+++ Temperatura/config.h
@@ -1,10 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define WLAN_SSID "Etec-IoT"
-#define WLAN_PASS "etecjb*123"
+#define WLAN_SSID "SUA_REDE_SEM_FIO"
+#define WLAN_PASS "SENHA_DA_REDE"
 
-#define HOSTNAME "professor"
+#define HOSTNAME "NOME_DO_GRUPO"
 
 #define DHT_PIN D6
 #define DHT_TYPE DHT11
